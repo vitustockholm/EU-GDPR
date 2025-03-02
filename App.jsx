67b8a278d0aac12e24795f7648060e3d075dc64f
Vitus <83448449+vitustockholm@users.noreply.github.com>
2025-03-02 20:50:55 +0200

    Create App.jsx
    Import the cookie consent modal into your App.jsx:

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CookieConsent from "./components/CookieConsent";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Website</h1>
+      <CookieConsent />
+    </div>
+  );
+};
+
+export default App;
